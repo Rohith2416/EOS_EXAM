c0a1c8f7a5b755b2a3228dd8c95cc50350765660
Rohith2416 <erohith8055@gmail.com>
2021-12-14 12:42:42 +0530

    exam

---
+++ p2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<sys/mman.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+int *count;
+
+int main()
+{
+    int shmd;
+    shmd=shm_open("/cdacshm",O_RDWR | O_CREAT,S_IRUSR | S_IWUSR);
+
+    ftruncate(shmd,sizeof(int));
+    count=(int*)mmap(NULL,sizeof(int),PROT_READ | PROT_WRITE,MAP_SHARED,shmd,0);
+    while(1)
+    {
+        scanf("count :%d\n",*count);
+        sleep(1);
+
+    }
+    return 0;
+    
+}
